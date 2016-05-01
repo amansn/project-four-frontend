7f331f21c47a589293716f5ebadcd5bbe5cb86f1
Aman <amanjeet@gmail.com>
2016-05-01 17:12:00 -0400

    Setup

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const Home = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>COPP'D</h1>
+      </div>
+    )
+  }
+});
+
+export default Home;
